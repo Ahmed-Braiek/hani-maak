658a64bcb8ac1477263b55a14cab4940d6a9a851
hani-maak-ci <actions@users.noreply.github.com>
2026-09-26 00:36:23 +0000

    Build Flutter caregiver preview [skip flutter]

--- apps/web/public/flutter/flutter_bootstrap.js
+++ apps/web/public/flutter/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "71882442" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "347326675" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
